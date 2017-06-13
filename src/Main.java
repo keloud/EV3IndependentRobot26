95cf6b09e345a2241eb9f173b886eab6f63f8169
Keloud <toplaykisk@gmail.com>
2017-06-14 01:28:50 +0900

    リファクタリング

--- src/Main.java
+++ src/Main.java
@@ -6,35 +6,47 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
 
 public class Main {
-    // 超音波センサー
-    private static final EV3UltrasonicSensor sonar = new EV3UltrasonicSensor(SensorPort.S2);
-
     // カラーセンサー
     private static final EV3ColorSensor color = new EV3ColorSensor(SensorPort.S1);
+
+    // 超音波センサー
+    private static final EV3UltrasonicSensor sonar = new EV3UltrasonicSensor(SensorPort.S2);
 
     // ジャイロセンサー
     private static final EV3GyroSensor gyro = new EV3GyroSensor(SensorPort.S3);
 
-    // モーター
+    // モーター登録
     final RegulatedMotor motorCenter = Motor.A;
     final RegulatedMotor motorLeft = Motor.B;
     final RegulatedMotor motorRight = Motor.C;
-    // 車両情報
     // タイヤ直径(cm)
     private final float wr = 5.6F;
     // 車輪の幅
     private final float track = 9.2F;
-    //基礎計算情報
     // 円周率
     private final double pi = Math.PI;
+    // モーター角度
     private int degreeCenter = 0, degreeLeft = 0, degreeRight = 0;
 
-    public static void main(String[] args) {
+    private Main() {
+        for (int i = 0; i < 1500; i++) {
+            sonarUpdate();
+            colorUpdate();
+            gyroUpdate();
+            Battery.getVoltageMilliVolt();
+        }
+        Delay.msDelay(10000);
+        lcdUpdate();
         sonar.enable();
         color.getColorIDMode();
         gyro.getAngleMode();
+    }
+
+    public static void main(String[] args) {
+        new Main();
     }
 
     private void lcdUpdate() {
